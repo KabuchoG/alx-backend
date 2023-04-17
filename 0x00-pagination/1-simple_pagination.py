91086c172fe4fbcd7b9aec3d6127a9f1e0c320f2
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-17 15:17:58 -0700

    updates

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-"""task 1"""
+"""Simple pagination sample.
+"""
 import csv
-import math
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """returns a tuple of size two containing a start index and an end index
+    """Retrieves the index range from a given page and page size.
     """
     start = (page - 1) * page_size
-    end = page * page_size
+    end = start * page_size
     return (start, end)
 
 
@@ -19,6 +19,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -33,16 +35,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Get a page of the dataset
-        Args:
-            page: the page number
-            page_size: the number of rows per page
-        Returns:
-            A list of rows
+        """Retrieves a page of data.
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
-        if start >= len(self.dataset()):
+        data = self.dataset()
+        if start > len(data):
             return []
-        return self.dataset()[start:end]
+        return data[start:end]
