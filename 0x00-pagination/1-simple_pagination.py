13ed9a095c32b0ab504ebb2d16d27a3f30955a39
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-17 14:58:10 -0700

    updates

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,7 +6,8 @@ from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """returns a tuple of size two containing a start index and an end index"""
+    """returns a tuple of size two containing a start index and an end index
+    """
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
