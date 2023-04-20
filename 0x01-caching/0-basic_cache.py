a5d931aee6825b66ab98a496e3dbaca8091e0ef0
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-20 05:29:44 -0700

    new ocmmit

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,12 +7,14 @@ class BasicCache(BaseCaching):
     """
     A BasicCache class
     """
+
     def put(self, key, item):
         """
         Add an item in the cache
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
+
     def get(self, key):
         """
         Get an item by key
