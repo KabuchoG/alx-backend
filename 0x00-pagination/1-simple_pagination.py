28e9d797630ee94311df35fae47419ce71a4168d
Kabucho G <kabuchogeorge3@gmail.com>
2023-04-20 01:32:51 -0700

    corrections

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -9,7 +9,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """returns a tuple of size two containing a start index and an end index
     """
     start = (page - 1) * page_size
-    end = page * page_size
+    end = start + page_size
     return (start, end)
 
 
